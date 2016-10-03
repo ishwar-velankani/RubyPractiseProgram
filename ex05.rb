7b3c6ecd60b95c5dcb9b0abb86b7b0bdbfbab4a9
Ishwar Kumar <ishwar-velankan@velankani.com>
2016-10-03 12:08:54 +0530

    arranged

---
+++ ex05.rb
@@ -0,0 +1,11 @@
+my_name = 'Ishwar Kumar'
+my_age = 35
+my_height =74
+my_eyes = 'Blue'
+my_teeth = 'white'
+my_hair = 'black'
+
+puts "lets talk about #{my_name}"
+puts "he's #{my_height} inches tall"
+puts "he has got #{my_hair}"
+puts "if i add #{my_age}, #{my_height}, and if #{my_age}"
