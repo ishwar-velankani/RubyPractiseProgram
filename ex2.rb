5667c2a93fd6d5f3729e2ab607d953176a44a5b6
Ishwar Kumar <ishwar-velankan@velankani.com>
2016-10-03 12:07:14 +0530

    arranging the file in order

--- ex2.rb
+++
@@ -1,6 +0,0 @@
-#A comment, this is to ignoered
-#anythimhg after this # is ignored by ruby
-
-puts " I could have code like this" #and the comment after the program is ignored
-#comment can also be used to disable the command
-#puts "hello"
